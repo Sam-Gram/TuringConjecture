ce4268adcee0ba6e4795a408c9cb019891895359
Sam Graham <samgraham@outlook.com>
2016-02-16 10:58:08 -0700

    Weird State

--- States.py
+++ States.py
@@ -9,16 +9,14 @@ transition_function = {("second","0"):("second", "0", "R"),
                        ("fourth", " "):("final", " ", "N"),
                        ("fourth", "1"):("fifth", "1", "R"), # We now need to multiply by 3 and add 1
                        ("fourth", "0"):("fifth", "0", "R"), # Pretty sure we can delete this
-                       ("fifth", "1"):("sixth", "1", "R"),
-                       ("sixth", " "):("seventh", "0", "L"),
-                       ("seventh", "1"):("eighth", "1", "R"),
-                       ("eighth", "0"):("ninth","1","L"),
-                       ("ninth","0"):("tenth","0","L"),
-                       ("ninth","1"):("eleventh","1","L"),
-                       ("ninth"," "):("init"," ","N"), #Restart on new number
-                       ("tenth","0"):("ninth","0","N"),
-                       ("eleventh"," "):("init"," ","N"),
-                       ("eleventh","1"):("eleventh", "1", "R"),
-                       ("twelfth","1"):("ninth", "0", "L")
+                       ("fifth", "1"):("sixth","0","L"), 
+                       ("sixth", "0"):("carry-add-one","0","R"),
+                       ("sixth", "1")
+                       ("carry-add-one", "1")
+                       ("move-left", " "):()
+                       ("move-left", "1"):()
+                       ("move-left", "0"):()
+                       
+                       
                        }
 final_states = ["final"]
